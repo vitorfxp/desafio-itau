2dc3cf2603658b39dde9be1114d240ca5d62774a
João Vitor Ferreira Ribeiro <02788678207@sedam.ro.gov.br>
2026-07-03 10:20:40 -0400

    First commit

---
+++ src/main/java/dev/jv/DesafioItau/DesafioItauApplication.java
@@ -0,0 +1,13 @@
+package dev.jv.DesafioItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauApplication.class, args);
+	}
+
+}
