ecff425d33af0ee903f8ea2313bc9cbcfb825e7e
João Vitor Ferreira Ribeiro <02788678207@sedam.ro.gov.br>
2026-07-09 12:05:59 -0400

    CHORE: Mock de operações

--- src/main/java/dev/jv/DesafioItau/repository/TransacaoRepository.java
+++ src/main/java/dev/jv/DesafioItau/repository/TransacaoRepository.java
@@ -1,9 +1,12 @@
 package dev.jv.DesafioItau.repository;
 
+import dev.jv.DesafioItau.dto.EstatiscasDTO;
 import dev.jv.DesafioItau.dto.TransacaoDTO;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
 @Repository
@@ -21,6 +24,18 @@ public class TransacaoRepository {
 
     public void deletarDados(){
         dataList.clear();
+    }
+
+    public EstatiscasDTO estatisticas(OffsetDateTime horaAtual){
+        // Terminar os metodos de calcular estatisticas
+        DoubleSummaryStatistics operacoes = new DoubleSummaryStatistics();
+//        Long count = dataList.stream().count();
+//        double avg =  dataList.stream(). /count;
+//        double min = dataList.stream().min();
+       EstatiscasDTO dados = new EstatiscasDTO(23L, 23,23,23,23);
+
+       return dados;
+
     }
 
 }
